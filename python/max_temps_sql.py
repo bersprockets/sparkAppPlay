9977b946ef92b5bf7840bee9ccf5cb3517a3c114
Bruce Robbins <bersprockets@gmail.com>
2018-02-04 16:05:36 -0800

    Fix up python script

--- python/max_temps_sql.py
+++ python/max_temps_sql.py
@@ -2,6 +2,7 @@ import sys
 import pyspark.sql.session as session
 import string
 from pyspark.sql.types import *
+from pyspark.sql.functions import substring
 
 format = sys.argv[1]
 input_filename = sys.argv[2]
@@ -13,11 +14,12 @@ spark = session.SparkSession \
                .appName("maxTempsSql4") \
                .getOrCreate()
 
-station_fields = [StructField("stationName", StringType()), StructField("country", StringType())]
-station_schema = StructType(station_fields)
-stations = spark.read.schema(station_schema).json(station_filename)
+stations_raw = spark.read \
+                    .text(station_filename) \
+                    .where("length(value) > 0 and substring(value, 1 ,1) >= '0' and substring(value, 1, 1) <= '9'")
 
-
+stations = stations_raw.select(substring(stations_raw.value, 1, 12).alias("stationName"), \
+                               substring(stations_raw.value, 44, 2).alias("country"))
 input_fields = [StructField("stationName", StringType()), StructField("year", IntegerType()),\
                 StructField("month", IntegerType()), StructField("day", IntegerType()),\
                 StructField("temperature", DoubleType())]
