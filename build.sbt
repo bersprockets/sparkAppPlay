8228ddf9b6017bee82d54ebf492c513d35821ac3
Bruce Robbins <bersprockets@gmail.com>
2018-02-23 08:53:02 -0800

    Add kudu and pre-filter cases

--- build.sbt
+++ build.sbt
@@ -6,3 +6,4 @@ scalaVersion := "2.11.8"
 
 libraryDependencies += "org.apache.spark" %% "spark-core" % "2.2.1" % "provided"
 libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.2.1" % "provided"
+libraryDependencies += "org.apache.kudu" %% "kudu-spark2" % "1.2.0" % "provided"
--- src/main/scala/pushedFilterTest.scala
+++ src/main/scala/pushedFilterTest.scala
@@ -2,8 +2,13 @@ import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.types._
 import org.apache.spark.sql.functions._
 
+import org.apache.kudu.spark.kudu._
+import org.apache.kudu.client._
+import collection.JavaConverters._
+
 object pushedFilterTest {
   case class Record(val randid2: Long, val randid1: Long)
+  case class KuduRecord(val pk: Long, val randid1: Long, val randid2: Long)
 
   def main(args: Array[String]): Unit = {
     if (args.length < 1) {
@@ -27,6 +32,12 @@ object pushedFilterTest {
         dsFilter(args, spark, schema)
       case "dfFilter" =>
         dfFilter(args, spark, schema)
+      case "dsJoinFilterGroupByKudu" =>
+        dsJoinFilterGroupByKudu(args, spark)
+      case "dfJoinFilterGroupByKudu" =>
+        dfJoinFilterGroupByKudu(args, spark)
+      case "dsJoinPreFilterGroupBy" =>
+        dsJoinPreFilterGroupBy(args, spark, schema)
       case _ => Console.err.println("What???")
     }
 
@@ -235,6 +246,137 @@ object pushedFilterTest {
 
     val startTime = System.currentTimeMillis()
     println(resultDf.count)
+    val interval = System.currentTimeMillis() - startTime
+    println(s"Interval is $interval")
+  }
+
+  def dsJoinFilterGroupByKudu(args: Array[String], spark: SparkSession): Unit = {
+    if (args.length < 2) {
+      Console.err.println("No kudu master specified")
+      System.exit(1)
+    }
+    val master = args(1);
+
+    if (args.length < 3) {
+      Console.err.println("No left kudu table specified")
+      System.exit(1)
+    }
+    val leftTableName = args(2);
+
+    if (args.length < 4) {
+      Console.err.println("No right kudu table specified")
+      System.exit(1)
+    }
+    val rightTableName = args(3);
+
+    import spark.implicits._
+
+    val table1Df = spark
+      .read
+      .options(Map("kudu.master" -> master, "kudu.table" -> leftTableName))
+      .kudu
+    val table1 = table1Df.as[KuduRecord]
+
+    val table2Df = spark
+      .read
+      .options(Map("kudu.master" -> master, "kudu.table" -> rightTableName))
+      .kudu
+    val table2 = table2Df.as[KuduRecord]
+
+    val resultDs = table1
+      .joinWith(table2, table2.col("randid2") === table1.col("randid1"))
+      .filter("_2.randid2 > 30")
+      .groupBy("_1.randid1")
+      .agg(count("*") as "numOccurances")
+
+    resultDs.explain
+
+    val startTime = System.currentTimeMillis()
+    println(resultDs.count)
+    val interval = System.currentTimeMillis() - startTime
+    println(s"Interval is $interval")
+  }
+
+  def dfJoinFilterGroupByKudu(args: Array[String], spark: SparkSession): Unit = {
+    if (args.length < 2) {
+      Console.err.println("No kudu master specified")
+      System.exit(1)
+    }
+    val master = args(1);
+
+    if (args.length < 3) {
+      Console.err.println("No left kudu table specified")
+      System.exit(1)
+    }
+    val leftTableName = args(2);
+
+    if (args.length < 4) {
+      Console.err.println("No right kudu table specified")
+      System.exit(1)
+    }
+    val rightTableName = args(3);
+
+    import spark.implicits._
+
+    val table1 = spark
+      .read
+      .options(Map("kudu.master" -> master, "kudu.table" -> leftTableName))
+      .kudu
+
+    val table2 = spark
+      .read
+      .options(Map("kudu.master" -> master, "kudu.table" -> rightTableName))
+      .kudu
+
+    val resultDf = table1
+      .join(table2, table2.col("randid2") === table1.col("randid1"))
+      .filter(table2("randid2") >"30")
+      .groupBy(table1("randid1"))
+      .agg(count("*") as "numOccurances")
+
+    resultDf.explain
+
+    val startTime = System.currentTimeMillis()
+    println(resultDf.count)
+    val interval = System.currentTimeMillis() - startTime
+    println(s"Interval is $interval")
+  }
+
+  def dsJoinPreFilterGroupBy(args: Array[String], spark: SparkSession, schema: StructType): Unit = {
+    if (args.length < 2) {
+      Console.err.println("No input file format specified")
+      System.exit(1)
+    }
+    val format = args(1);
+
+    if (args.length < 2) {
+      Console.err.println("No left input file format specified")
+      System.exit(1)
+    }
+    val leftInputFilename = args(2);
+
+    if (args.length < 4) {
+      Console.err.println("No right input file specified")
+      System.exit(1)
+    }
+    val rightInputFilename = args(3);
+
+    import spark.implicits._
+
+    val table1 = spark.read.schema(schema).format(format).load(leftInputFilename).as[Record]
+    val table2 = spark.read.schema(schema).format(format).load(rightInputFilename).as[Record]
+
+    val table1Filtered = table1.filter("randid1 > 30")
+    val table2Filtered = table2.filter("randid2 > 30")
+    val resultDs = table1Filtered
+      .joinWith(table2Filtered, table2Filtered.col("randid2") === table1Filtered.col("randid1"))
+      .groupBy("_1.randid1")
+      .agg(count("*") as "numOccurances")
+
+    resultDs.explain
+
+    val startTime = System.currentTimeMillis()
+    println(resultDs.count)
     val interval = System.currentTimeMillis() - startTime
     println(s"Interval is $interval")
   }
